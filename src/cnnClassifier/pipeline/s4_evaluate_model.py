69da9261a6433a63bfff3704dcd2d3053cc06b58
asbpintu <asbpintu@gmail.com>
2023-12-18 22:49:42 +0530

    cicd pipeline added

--- src/cnnClassifier/pipeline/s4_evaluate_model.py
+++ src/cnnClassifier/pipeline/s4_evaluate_model.py
@@ -17,7 +17,7 @@ class EvaluateModelPipeline:
         evaluation = Evaluation(eval_config)
         evaluation.evaluation()
         evaluation.save_score()
-        evaluation.log_into_mlflow()
+        # evaluation.log_into_mlflow()
 
 
 
